0567e20457bf7249762a0d1d4621b851809bda04
Hoang Tran <hoang.tran@uclouvain.be>
2017-01-03 17:25:45 +0100

    MacOS: fix: enable IP forwarding on host
    credit: @ginoledesma found this first

--- user_scripts/halt/90_nat.sh
+++ user_scripts/halt/90_nat.sh
@@ -8,6 +8,7 @@ echo "==> Disabling IP Masquerading"
 if [[ "$uname_str" == "Linux" ]]; then
 	sudo iptables -t nat -D POSTROUTING -s 192.168.33.0/24 -j MASQUERADE
 elif [[ "$uname_str" == "Darwin" ]]; then
+	sudo sysctl -w net.inet.ip.forwarding=0
 	sudo pfctl -df /etc/pf.conf > /dev/null 2>&1;
 else
 	echo "FAILED! Host OS unknown"
--- user_scripts/up/10_nat.sh
+++ user_scripts/up/10_nat.sh
@@ -8,6 +8,7 @@ echo "==> Enabling IP Masquerading"
 if [[ "$uname_str" == "Linux" ]]; then
 	sudo iptables -t nat -A POSTROUTING -s 192.168.33.0/24 -j MASQUERADE
 elif [[ "$uname_str" == "Darwin" ]]; then
+	sudo sysctl -w net.inet.ip.forwarding=1
 	echo "nat on en0 from 192.168.33.0/24 to any -> en0" | sudo pfctl -ef - >/dev/null 2>&1; 
 else
 	echo "FAILED! Host OS unknown"
