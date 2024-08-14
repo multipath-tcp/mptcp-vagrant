7d1d920bdddb20e75cc88d3a476307f4e14c63b8
Aperence <anthony.doeraene@hotmail.com>
2024-08-14 14:01:26 +0200

    even further changes

--- scripts/init-mptcp.sh
+++ scripts/init-mptcp.sh
@@ -2,9 +2,9 @@ echo "Setting up MPTCP..."
 
 vagrant ssh client <<EOF
     # Sometimes the ip addresses aren't assigned by virtualbox, so do it manually
-    sudo ip addr add 192.168.56.100/24 dev eth1
-    sudo ip addr add 192.168.57.100/24 dev eth2
-    sudo ip addr add 192.168.58.100/24 dev eth3
+    sudo ip addr add 192.168.56.100/24 dev eth1 2> /dev/null
+    sudo ip addr add 192.168.57.100/24 dev eth2 2> /dev/null
+    sudo ip addr add 192.168.58.100/24 dev eth3 2> /dev/null
 
     # Configure MPTCP
     sudo sysctl net.mptcp.enabled=1
@@ -16,15 +16,15 @@ EOF 2> /dev/null
 
 vagrant ssh server <<EOF
     # Sometimes the ip addresses aren't assigned by virtualbox, so do it manually
-    sudo ip addr add 192.168.56.101/24 dev eth1
-    sudo ip addr add 192.168.57.101/24 dev eth2
-    sudo ip addr add 192.168.58.101/24 dev eth3
+    sudo ip addr add 192.168.56.101/24 dev eth1 2> /dev/null
+    sudo ip addr add 192.168.57.101/24 dev eth2 2> /dev/null
+    sudo ip addr add 192.168.58.101/24 dev eth3 2> /dev/null
 
     # Configure MPTCP
     sudo sysctl net.mptcp.enabled=1
     sudo ip mptcp limits set subflow 3
     sudo ip mptcp endpoint add 192.168.57.101 dev eth2 signal
     sudo ip mptcp endpoint add 192.168.58.101 dev eth3 signal
-EOF 2> /dev/null
+EOF
 
 echo "Done setting up MPTCP..."
