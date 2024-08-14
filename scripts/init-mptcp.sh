82160e64113c8682267b46b3d61e7d2e05bf7c6d
Aperence <anthony.doeraene@hotmail.com>
2024-08-14 13:58:02 +0200

    further changes

--- scripts/init-mptcp.sh
+++ scripts/init-mptcp.sh
@@ -8,8 +8,8 @@ vagrant ssh client <<EOF
 
     # Configure MPTCP
     sudo sysctl net.mptcp.enabled=1
-    sudo ip mptcp limits set subflow 9
-    sudo ip mptcp limits set add_addr_accepted 9
+    sudo ip mptcp limits set subflow 3
+    sudo ip mptcp limits set add_addr_accepted 3
     sudo ip mptcp endpoint add 192.168.57.100 dev eth2 subflow
     sudo ip mptcp endpoint add 192.168.58.100 dev eth3 subflow
 EOF 2> /dev/null
@@ -22,7 +22,7 @@ vagrant ssh server <<EOF
 
     # Configure MPTCP
     sudo sysctl net.mptcp.enabled=1
-    sudo ip mptcp limits set subflow 9
+    sudo ip mptcp limits set subflow 3
     sudo ip mptcp endpoint add 192.168.57.101 dev eth2 signal
     sudo ip mptcp endpoint add 192.168.58.101 dev eth3 signal
 EOF 2> /dev/null
